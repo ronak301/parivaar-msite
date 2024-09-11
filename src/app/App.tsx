293b428619f63fc191546fe43d1707c3f9a19a07
Ronak Kothari <ronakkothari301@gmail.com>
2024-09-11 09:38:40 +0530

    adding privacy policy and t&c

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,8 @@ import MembersList from "../modules/directory/screens/MembersList.tsx";
 import { useSelector } from "react-redux";
 import { isEmpty } from "lodash";
 import { RootState } from "./store.ts";
+import Privacy from "../modules/authentication/screens/Privacy";
+import Terms from "../modules/authentication/screens/Terms";
 
 const App = () => {
   const accessToken = useSelector(
@@ -15,6 +17,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginScreen />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/terms" element={<Terms />} />
       <Route
         path="/"
         element={isLoggedIn ? <MembersList /> : <Navigate to="/login" />}
